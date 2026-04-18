94713c8ec909b15644b3de897ab536522ddd99f7
wuyucun <wyc1996@gmail.com>
2026-04-18 18:33:40 +0800

    new

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,19 +6,63 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: '望云川文档',
+			description: '人生笔记本，归纳碎片化信息流，提供极致流畅的阅读体验。',
+			defaultLocale: 'zh-CN',
+			locales: {
+				root: {
+					label: '中文',
+					lang: 'zh-CN',
+				},
+			},
+			lastUpdated: true,
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+	     	//sidebar: [
+			//	{
+		//			label: '导航',
+	//				items: [
+						// Each item here is one entry in the navigation menu.
+		//				{ label: 'Example Guide', slug: 'guides/example' },
+			//		],
+			//		  autogenerate: {
+			//			  directory: '.',
+			//			  collapsed: true // ✅ 所有子目录默认折叠
+			//	  },
+	//			{
+	//				label: 'Reference',
+		//			autogenerate: { directory: 'health' },
+
+			//	  },
+
+			//	],
+
+
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+					label: '健康/健身',
+					collapsed: true,
+					// 自动扫描 src/content/docs/ 下所有文件/文件夹
+					autogenerate: {
+						directory: 'health',
+					},
 				},
+
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: '投资理财',
+					collapsed: true,
+					// 自动扫描 src/content/docs/ 下所有文件/文件夹
+					autogenerate: {
+						directory: 'invest',
+					},
+				},
+
+				{
+					label: '技术与生活',
+					collapsed: true,
+					// 自动扫描 src/content/docs/ 下所有文件/文件夹
+					autogenerate: {
+						directory: 'lifedoc',
+					},
 				},
 			],
 		}),
